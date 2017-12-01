8fcec5f69325f925c270eac844761f503515506e
Cora Iberkleid <ciberkleid@pivotal.io>
2017-12-01 08:30:25 -0500

    set scripts_output_folder

--- task.sh
+++ task.sh
@@ -8,22 +8,40 @@ export ROOT_FOLDER
 ROOT_FOLDER="$( pwd )"
 export REPO_RESOURCE=repo
 export TOOLS_RESOURCE=tools
-export VERSION_RESOURCE=version
 export OUTPUT_RESOURCE=out
 
 echo "Root folder is [${ROOT_FOLDER}]"
 echo "Repo resource folder is [${REPO_RESOURCE}]"
 echo "Tools resource folder is [${TOOLS_RESOURCE}]"
-echo "Version resource folder is [${VERSION_RESOURCE}]"
 
 # If you're using some other image with Docker change these lines
 # shellcheck source=/dev/null
 source /docker-lib.sh || echo "Failed to source docker-lib.sh... Hopefully you know what you're doing"
 start_docker || echo "Failed to start docker... Hopefully you know what you're doing"
 
-# shellcheck source=/dev/null
-source "${ROOT_FOLDER}/${TOOLS_RESOURCE}/concourse/tasks/pipeline.sh"
+#!/bin/bash
 
+set -o errexit
+set -o errtrace
+set -o pipefail
+
+
+# Contents of pipeline.sh
+export SCRIPTS_OUTPUT_FOLDER="${ROOT_FOLDER}/${REPO_RESOURCE}/ciscripts"
+echo "Scripts will be copied to [${SCRIPTS_OUTPUT_FOLDER}]"
+
+echo "Copying pipelines scripts"
+cd "${ROOT_FOLDER}/${REPO_RESOURCE}" || exit
+mkdir -p "${SCRIPTS_OUTPUT_FOLDER}" || echo "Failed to create the scripts output folder"
+[[ -d "${ROOT_FOLDER}/${TOOLS_RESOURCE}/" ]] && \
+    cp -r "${ROOT_FOLDER}/${TOOLS_RESOURCE}"/*_tag.sh "${SCRIPTS_OUTPUT_FOLDER}"/ || \
+    echo "Failed to copy the scripts"
+
+export CI="CONCOURSE"
+
+export TERM=dumb
+
+# Continue with task
 echo "${MESSAGE}"
 cd "${ROOT_FOLDER}/${REPO_RESOURCE}" || exit
 
