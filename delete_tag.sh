440c907d1a5d39af7eb7955784fba2615242af8a
Cora Iberkleid <ciberkleid@pivotal.io>
2017-12-01 08:48:20 -0500

    minor correction

--- delete_tag.sh
+++ delete_tag.sh
@@ -11,10 +11,9 @@ if [[ ! -z "${LATEST_TEST_TAG}" ]]; then
 	echo "LATEST_TEST_TAG already set to [${LATEST_TEST_TAG}}]. Returning same value."
 	echo "${LATEST_TEST_TAG}"
 else
-	local latestTestTag
-	latestTestTag=$("${GIT_BIN}" for-each-ref --sort=taggerdate --format '%(refname)' refs/tags/test | tail -1)
 	export LATEST_TEST_TAG
-	LATEST_TEST_TAG="${latestTestTag#refs/tags/}"
+	LATEST_TEST_TAG=$("${GIT_BIN}" for-each-ref --sort=taggerdate --format '%(refname)' refs/tags/test | tail -1)
+	LATEST_TEST_TAG="${LATEST_TEST_TAG#refs/tags/}"
 	echo "${LATEST_TEST_TAG}"
 fi
 echo "Latest test tag is [${LATEST_TEST_TAG}]"
