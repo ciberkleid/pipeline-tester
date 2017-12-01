0ac09bd736b9ee612ab3a3c0b65b7d533548688f
Cora Iberkleid <ciberkleid@pivotal.io>
2017-12-01 10:15:08 -0500

    trying stuff

--- delete_tag.sh
+++ delete_tag.sh
@@ -24,9 +24,9 @@ echo
 
 tagName="test/${PIPELINE_VERSION}"
 echo "Deleting test tag [${tagName}]"
-//if [[ "${CI}" != "CONCOURSE" ]]; then
-	git push --delete origin "${tagName}"
-//fi
+
+git push --delete origin "${tagName}"
+
 git tag -d "${tagName}"
 
 echo "Run: git tag"
