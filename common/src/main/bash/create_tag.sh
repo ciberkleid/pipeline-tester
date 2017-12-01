54d2b054056cf217f4010f31b39f9629acb60c02
Cora Iberkleid <ciberkleid@pivotal.io>
2017-12-01 08:14:19 -0500

    cleanup

--- common/src/main/bash/create_tag.sh
+++ common/src/main/bash/create_tag.sh
@@ -4,14 +4,6 @@ set -o errexit
 
 __DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-export ENVIRONMENT=PROD
-
-# shellcheck source=/dev/null
-[[ -f "${__DIR}/pipeline.sh" ]] && source "${__DIR}/pipeline.sh" ||  \
- echo "No pipeline.sh found"
-
-#performProductionDeployment
-
 echo "Tagging the project with test tag"
 echo "test/${PIPELINE_VERSION}" > "${ROOT_FOLDER}/${REPO_RESOURCE}/tag"
 cp -r "${ROOT_FOLDER}/${REPO_RESOURCE}"/. "${ROOT_FOLDER}/${OUTPUT_RESOURCE}/"
--- common/src/main/bash/delete_tag.sh
+++ common/src/main/bash/delete_tag.sh
@@ -4,12 +4,6 @@ set -o errexit
 
 __DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-export ENVIRONMENT=PROD
-
-# shellcheck source=/dev/null
-[[ -f "${__DIR}/pipeline.sh" ]] && source "${__DIR}/pipeline.sh" ||  \
- echo "No pipeline.sh found"
-
 # repo may contain the new repo or the prod-tagged repo
 # in case it is the latter, get the latest prod tag
 # if it is the former, this will return nothing
