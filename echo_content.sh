4aeb3bf9f3a7e355a280b30d5de729f368a4d423
Cora Iberkleid <ciberkleid@pivotal.io>
2017-12-01 18:28:12 -0500

    trying stuff

--- echo_content.sh
+++ echo_content.sh
@@ -9,6 +9,20 @@ cat manifest.yml
 
 git tag
 
+if [[ ! -z "${LATEST_PROD_TAG}" ]]; then
+    echo "LATEST_PROD_TAG already set to [${LATEST_PROD_TAG}}]. Returning same value."
+	echo "${LATEST_PROD_TAG}"
+else
+	latestProdTag=$(git for-each-ref --sort=taggerdate --format '%(refname)' refs/tags/prod | tail -1)
+	export LATEST_PROD_TAG
+	LATEST_PROD_TAG="${latestProdTag#refs/tags/}"
+	echo "${LATEST_PROD_TAG}"
+fi
+
+git branch
+git checkout "${LATEST_PROD_TAG}"
+git branch
+
 exit 0
 
 
